03d2cfc9239f2f99b7aa785ace23dc9c1676787e
Bhushan <bhushanforu74@gmail.com>
2024-04-24 13:01:57 -0400

    new sql

--- manager_vibushan.sql
+++ manager_vibushan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 15, 2024 at 12:23 AM
+-- Generation Time: Apr 24, 2024 at 01:00 PM
 -- Server version: 10.6.16-MariaDB-0ubuntu0.22.04.1
 -- PHP Version: 8.1.2-1ubuntu2.14
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `manager_vibushan` (
   `manager_id` int(11) NOT NULL,
   `email` varchar(30) NOT NULL,
-  `password` varchar(30) NOT NULL
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -38,14 +38,10 @@ CREATE TABLE `manager_vibushan` (
 --
 
 INSERT INTO `manager_vibushan` (`manager_id`, `email`, `password`) VALUES
-(1, 'bshelke1@vibushan.com', '123'),
-(2, 'v1@vibushan.com', '123'),
-(3, 'vijay@vibhusan.com', '123456'),
-(4, 'vijay@vibhusan.com', '123456'),
-(5, 'vijay@vibhusan.com', '123456'),
-(6, 'bshelke2@vibushan.com', '123'),
-(7, 'bshelke3@vibushan.com', '123'),
-(8, 'b1@vibushan.com', '123');
+(16, 'a2@vibushan.com', '$2y$10$UHWSZLZmBZ/J31djL.NAS.EQckxRKXUpxzaQrPjXet8A0awxlzucq'),
+(17, 'a2@vibushan.com', '$2y$10$aKCtu7zbtU7/h4kPE2.kTu7uDWW8gyD8gYfnyBAxpNYqDp.N2.ub.'),
+(18, 'sri@vibushan.com', '464646'),
+(19, 'a3@vibushan.com', '$2y$10$nlbpSRY6znrpb.DNg2kev.zyc.HS1RVrbSEiUD.kqTdGkg1wPB7wG');
 
 --
 -- Indexes for dumped tables
@@ -65,7 +61,7 @@ ALTER TABLE `manager_vibushan`
 -- AUTO_INCREMENT for table `manager_vibushan`
 --
 ALTER TABLE `manager_vibushan`
-  MODIFY `manager_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `manager_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
